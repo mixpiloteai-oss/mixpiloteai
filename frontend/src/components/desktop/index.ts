2a61d6aa4e82fb4d69bd3875d931cd6ac5cb522b
mixpiloteai-oss <mixpiloteai@gmail.com>
2026-05-16 22:56:25 +0200

    feat(desktop): add barrel export for desktop components

---
+++ frontend/src/components/desktop/index.ts
@@ -0,0 +1,5 @@
+// Desktop-only component barrel exports
+export { TitleBar } from './TitleBar';
+export { MidiPanel } from './MidiPanel';
+export { VSTPanel } from './VSTPanel';
+export { SystemInfoPanel } from './SystemInfo';
